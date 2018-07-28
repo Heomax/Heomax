f09648b9a266bb280ddd134c2e3139a4c72a2a39
Gustavo Ezequiel Aramburu <gunns.gnr@gmail.com>
2018-07-28 15:57:01 -0300

    added login and signup

--- HeoTag/HeoTag/config/environments/development.rb
+++ HeoTag/HeoTag/config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
       user_name: "no-reply",
       password: "Javier18"
   }
+
+  #devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 end
--- HeoTag/HeoTag/config/routes.rb
+++ HeoTag/HeoTag/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'subscriptions/index'
   get 'home/index'
 
